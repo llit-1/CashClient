8c15fa461a23347bbfaba103682e7840f0ab4d07
Unknown <it@ludilove.ru>
2023-06-16 12:10:25 +0300

    CashClient v1.4.4.0
    Сборка с новым RKNet_Model.dll

--- RKNet_CashClient/Models/CashClient.cs
+++ RKNet_CashClient/Models/CashClient.cs
@@ -8,6 +8,6 @@ namespace RKNet_CashClient.Models
 {
     internal class CashClient
     {
-        internal static string Version = "1.4.3.0";
+        internal static string Version = "1.4.4.0";
     }
 }
